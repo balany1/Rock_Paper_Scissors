f7a86f147709152eff5676314c6e32367dd5de33
Andrew McNamara <andrewmcnamara@live.co.ukandrewmcnamara@live.co.uk>
2022-06-27 20:11:39 +0100

    get winner function is defined

--- manual_rps.py
+++ manual_rps.py
@@ -1,3 +1,4 @@
+from ast import If
 import random
 
 class Rock_Paper_Scissors:
@@ -7,8 +8,8 @@ class Rock_Paper_Scissors:
         pass
 
     def get_computer_choice():
-        choice_list = ['Rock','Paper','Scissors','Nothing']
-        computer_choice = random.choice(choice_list)
+        choice_list = ['Rock','Paper','Scissors']
+        computer_choice = random.choice(choice_list).lower()
         return computer_choice
 
 
@@ -20,13 +21,32 @@ class Rock_Paper_Scissors:
                 return user_choice
             else:
                 print('Please choose one of the options selected')
-            
 
+    def get_winner(computer_choice,user_choice):
+        if computer_choice == user_choice:
+           print('It\'s a tie')
+        elif computer_choice == 'rock':
+            if user_choice == 'paper':
+                print('You won! Paper beats Rock')
+            else:
+                print('You lose! Rock beats Scissors')
+        elif computer_choice == 'scissors':
+            if user_choice == 'rock':
+                print('You won! Rock beats Scissors')
+            else:
+                print('You lose! Scissors beats Paper')  
+        elif computer_choice == 'paper':
+            if user_choice == 'scissors':
+                print('You won! Scissors beats Paper')
+            else:
+                print('You lose! Paper beats Rock')
+              
+            
+    
 
 
     if __name__ ==  '__main__':
         choice_list = ['Rock','Paper','Scissors','Nothing']
         computer_choice = get_computer_choice()
         user_choice = get_user_choice()
-        print(computer_choice)
-        print(user_choice)
+        get_winner(computer_choice,user_choice)
