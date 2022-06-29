190ea3d20a9e13e1de83ba3997fb13a72d66d6e1
Andrew McNamara <andrewmcnamara@live.co.ukandrewmcnamara@live.co.uk>
2022-06-29 17:56:17 +0100

    timer and score increments added

--- camera_rps.py
+++ camera_rps.py
@@ -1,6 +1,8 @@
 import cv2
 from keras.models import load_model
 import numpy as np
+import time
+from time import sleep
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
@@ -55,30 +57,48 @@ class Rock_Paper_Scissors:
         return user_choice
 
     def get_winner(self,computer_choice,user_choice):
+        user_score=0
+        computer_score=0
         if computer_choice == user_choice:
            print('It\'s a tie')
         elif computer_choice == 'rock':
             if user_choice == 'paper':
                 print('You won! Paper beats Rock')
+                user_score +=1
             else:
                 print('You lose! Rock beats Scissors')
+                computer_score += 1
         elif computer_choice == 'scissors':
             if user_choice == 'rock':
                 print('You won! Rock beats Scissors')
+                user_score +=1
             else:
-                print('You lose! Scissors beats Paper')  
+                print('You lose! Scissors beats Paper') 
+                computer_score += 1 
         elif computer_choice == 'paper':
             if user_choice == 'scissors':
                 print('You won! Scissors beats Paper')
+                user_score +=1
             else:
                 print('You lose! Paper beats Rock')
+                computer_score += 1
+        print(f"You have {user_score} points, the computer has {computer_score}")
 
 def play():
 
+    timer()
     game = Rock_Paper_Scissors()
     computer_choice = game.get_computer_choice()
     user_choice = game.get_user_choice()
     game.get_winner(computer_choice,user_choice)
+
+def timer():
+    countdown_timer = 3
+    while countdown_timer>0:
+        print(f"Show you hand in {countdown_timer} seconds")
+        sleep(1)
+        countdown_timer -= 1
+    print('GO!')
 
 if __name__ ==  '__main__':
     choice_list = ['Rock','Paper','Scissors','Nothing']
