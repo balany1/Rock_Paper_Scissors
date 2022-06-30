c506fd959c1e4b49b76408a8fa05e09096448da7
Andrew McNamara <andrewmcnamara@live.co.ukandrewmcnamara@live.co.uk>
2022-06-30 21:37:04 +0100

    Methods redefined and reordered

--- camera_rps.py
+++ camera_rps.py
@@ -1,3 +1,4 @@
+from typing_extensions import Self
 import cv2
 from keras.models import load_model
 import numpy as np
@@ -14,16 +15,12 @@ class Rock_Paper_Scissors:
 
     def __init__(self):
         #self.computer_choice = random.choice(self.choice_list)
+        self.user_score = 0
+        self.computer_score = 0
         pass
 
-    def get_computer_choice(self):
-        choice_list = ['Rock','Paper','Scissors']
-        computer_choice = random.choice(choice_list).lower()
-        #print(computer_choice)
-        return computer_choice
 
-
-    def get_user_choice(self):
+    def open_camera(self):
     
         while True: 
             ret, frame = cap.read()
@@ -31,10 +28,25 @@ class Rock_Paper_Scissors:
             image_np = np.array(resized_frame)
             normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
             data[0] = normalized_image
-            prediction = model.predict(data)
+            self.prediction = model.predict(data)
             cv2.imshow('frame', frame)
+
+            while self.user_score <3 and self.computer_score <3:
+                self.timer()
+                print(self.prediction)
+                user_choice = self.get_prediction()
+                print(user_choice)
+                computer_choice = self.get_computer_choice()
+                print(computer_choice)
+                self.get_winner(computer_choice,user_choice)
+                if self.user_score == 3: 
+                    print('You have won! What a hero')
+                    exit
+                elif self.computer_score == 3:
+                    print('You lost the game! Doofus')
+                    exit
+
             # Press q to close the window
-            print(prediction)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
             
@@ -43,67 +55,73 @@ class Rock_Paper_Scissors:
         # Destroy all the windows
         cv2.destroyAllWindows()
         
-        #determine the user choice from the values given by the model
-        if prediction[0][0] > 0.5:
+    def timer(self):
+        countdown_timer = 3
+        while countdown_timer>0:
+            print(f"Show you hand in {countdown_timer} seconds")
+            sleep(1)
+            countdown_timer -= 1
+        print('GO!')
+
+    def get_prediction(self):
+        if self.prediction[0][0] > 0.5:
             user_choice = 'rock'
-        elif prediction[0][1] > 0.5:
+        elif self.prediction[0][1] > 0.5:
             user_choice = 'paper'
-        elif prediction[0][2] > 0.5:
+        elif self.prediction[0][2] > 0.5:
             user_choice = 'scissors'
-        elif prediction[0][3] > 0.5:
+        elif self.prediction[0][3] > 0.5:
             user_choice = 'nothing'
-        
-        #print(user_choice)
+        else:
+            user_choice = 'No user choice detected'
         return user_choice
 
+    def get_computer_choice(self):
+        choice_list = ['Rock','Paper','Scissors']
+        computer_choice = random.choice(choice_list).lower()
+        #print(computer_choice)
+        return computer_choice
+
     def get_winner(self,computer_choice,user_choice):
-        user_score=0
-        computer_score=0
-        if computer_choice == user_choice:
-           print('It\'s a tie')
-        elif computer_choice == 'rock':
-            if user_choice == 'paper':
-                print('You won! Paper beats Rock')
-                user_score +=1
-            else:
-                print('You lose! Rock beats Scissors')
-                computer_score += 1
-        elif computer_choice == 'scissors':
-            if user_choice == 'rock':
-                print('You won! Rock beats Scissors')
-                user_score +=1
-            else:
-                print('You lose! Scissors beats Paper') 
-                computer_score += 1 
-        elif computer_choice == 'paper':
-            if user_choice == 'scissors':
-                print('You won! Scissors beats Paper')
-                user_score +=1
-            else:
-                print('You lose! Paper beats Rock')
-                computer_score += 1
-        print(f"You have {user_score} points, the computer has {computer_score}")
+        #while self.user_score <=3 and self.computer_score <=3:
+    
+            if computer_choice == user_choice:
+                print('It\'s a tie')
+            elif computer_choice == 'rock':
+                if user_choice == 'paper':
+                    print('You won! Paper beats Rock')
+                    self.user_score +=1
+                else:
+                    print('You lose! Rock beats Scissors')
+                    self.computer_score += 1
+            elif computer_choice == 'scissors':
+                if user_choice == 'rock':
+                    print('You won! Rock beats Scissors')
+                    self.user_score +=1
+                else:
+                    print('You lose! Scissors beats Paper') 
+                    self.computer_score += 1 
+            elif computer_choice == 'paper':
+                if user_choice == 'scissors':
+                    print('You won! Scissors beats Paper')
+                    self.user_score +=1
+                else:
+                    print('You lose! Paper beats Rock')
+                    self.computer_score += 1
+            print(f"You have {self.user_score} points, the computer has {self.computer_score}")
+    
+        
 
-def play():
-
-    timer()
-    game = Rock_Paper_Scissors()
-    computer_choice = game.get_computer_choice()
-    user_choice = game.get_user_choice()
-    game.get_winner(computer_choice,user_choice)
-
-def timer():
-    countdown_timer = 3
-    while countdown_timer>0:
-        print(f"Show you hand in {countdown_timer} seconds")
-        sleep(1)
-        countdown_timer -= 1
-    print('GO!')
+    def play():
+        
+        game = Rock_Paper_Scissors()
+        game.open_camera()
+       
 
 if __name__ ==  '__main__':
     choice_list = ['Rock','Paper','Scissors','Nothing']
-        
-    play()
+    
+    Rock_Paper_Scissors.play()
 
 
 
